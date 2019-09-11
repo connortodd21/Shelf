df53427b119e3994c743540811f741a87e5a6914
Connor Todd <todd37@purdue.edu>
2019-09-10 20:35:59 -0400

    Add logout functionality on frontend

--- frontend/Shelf/src/app/app.component.ts
+++ frontend/Shelf/src/app/app.component.ts
@@ -1,15 +1,26 @@
-import { Component } from '@angular/core';
-import { Router, UrlSerializer } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from './servies/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Shelf';
 
-  constructor(private router: Router) {
-
+  constructor(private authService: AuthService) {
   }
+
+  ngOnInit() {
+    this.authService.checkAuthenticationStatus();
+  }
+
+  logout() {
+    this.authService.logoutUser().then(res => {
+      this.authService.logout();
+    });
+  }
+
 }
--- frontend/Shelf/src/app/servies/auth.service.ts
+++ frontend/Shelf/src/app/servies/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { AuthData } from '../models/auth.data.model';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { Subject } from 'rxjs';
 
 const httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
@@ -12,10 +13,64 @@ const httpOptions = {
 
 export class AuthService {
 
+    private tokenTimer: any;
+    private token: string;
+    private isAuthenticated = false;
+    private authStatusListener = new Subject<boolean>();
+    responseLogin = 'NULL';
+
     constructor(private http: HttpClient, private router: Router) { }
 
     registerUser(email: string, username: string, password: string, birthday: string) {
-        const auth: AuthData = { email: email, username: username, password: password, birthday: birthday };
-        return this.http.post<Object>('http://localhost:8080/user/register', auth).toPromise();
+        const auth: AuthData = { email, username, password, birthday };
+        return this.http.post<object>('http://localhost:8080/user/register', auth).toPromise();
+    }
+
+    logoutUser() {
+        const user = localStorage.getItem('user');
+        return this.http.post<object>('http://localhost:8080/user/logout', user).toPromise();
+    }
+
+    logout() {
+        this.token = null;
+        this.isAuthenticated = false;
+        this.authStatusListener.next(false);
+        clearTimeout(this.tokenTimer);
+        localStorage.removeItem('token');
+        localStorage.removeItem('expiration');
+        localStorage.removeItem('user');
+        this.router.navigate(['/login']);
+    }
+
+    checkAuthenticationStatus() {
+        const authInfo = this.getAuthenticationData();
+        if (!authInfo) {
+            return;
+        }
+
+        const currentTime: Date = new Date();
+        const expiresIn = new Date(authInfo.expirationDate).getTime() - currentTime.getTime();
+        if (expiresIn > 0) {
+            this.token = authInfo.token;
+            this.isAuthenticated = true;
+            this.setAuthTimer(expiresIn / 1000);
+            this.authStatusListener.next(true);
+        }
+    }
+
+    getAuthenticationData() {
+        const token = localStorage.getItem('token');
+        const expirationDate = localStorage.getItem('expiration');
+        if (!token || !expirationDate) {
+            return;
+        }
+        return { token, expirationDate };
+    }
+
+    private setAuthTimer(duration: number) {
+        console.log('Setting timer: ' + duration);
+        this.tokenTimer = setTimeout(() => {
+            this.logout();
+        }, duration * 1000);
     }
 }
