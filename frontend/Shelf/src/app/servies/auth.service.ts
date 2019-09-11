377c6cf75486a5bdbd53b032b4b376d1d932c355
Connor Todd <todd37@purdue.edu>
2019-09-10 20:48:12 -0400

    Add login service in frontend

--- frontend/Shelf/src/app/servies/auth.service.ts
+++ frontend/Shelf/src/app/servies/auth.service.ts
@@ -3,6 +3,7 @@ import { AuthData } from '../models/auth.data.model';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { Subject } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 const httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
@@ -24,6 +25,42 @@ export class AuthService {
     registerUser(email: string, username: string, password: string, birthday: string) {
         const auth: AuthData = { email, username, password, birthday };
         return this.http.post<object>('http://localhost:8080/user/register', auth).toPromise();
+    }
+
+    async login(username: string, password: string) {
+        const userInfo: AuthData = {username, password, email: '', birthday: ''};
+        await this.http.post<object>('http://localhost:8080/user/login', userInfo, httpOptions).pipe(
+            map( response => {
+                const token = response.headers.get('token');
+                this.token = token;
+                if (token) {
+                    const expiresInDuration = 7200;
+                    this.setAuthTimer(expiresInDuration);
+                    this.isAuthenticated = true;
+                    this.authStatusListener.next(true);
+                    const now = new Date();
+                    const expirationDate = new Date(now.getTime() + expiresInDuration * 1000);
+                    this.responseLogin = 'complete';
+                    localStorage.setItem('token', token);
+                    localStorage.setItem('expiresIn', expirationDate.toISOString());
+                    window.location.replace('/home');
+                }
+            })
+        ).toPromise().catch( err => {
+            if (err.error.message === 'Bad request: Login user data is incomplete') {
+                this.responseLogin = 'badRequest';
+            } else if (err.error.message === 'Unauthorized: Password is incorrect') {
+               this.responseLogin = 'badPassword';
+               return 'badPassword';
+            } else if (err.error.message === 'Not Found: User does not exist') {
+                this.responseLogin = 'DNE';
+                return 'DNE';
+            } else {
+                this.responseLogin = 'failed';
+                return 'FAILED';
+            }
+        });
+        return this.responseLogin;
     }
 
     logoutUser() {
@@ -58,7 +95,7 @@ export class AuthService {
         }
     }
 
-    getAuthenticationData() {
+    private getAuthenticationData() {
         const token = localStorage.getItem('token');
         const expirationDate = localStorage.getItem('expiration');
         if (!token || !expirationDate) {
