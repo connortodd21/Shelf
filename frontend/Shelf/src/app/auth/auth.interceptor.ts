5339460f8055a90d243c0f55e4cdf88ec6b9210a
Connor Todd <todd37@purdue.edu>
2019-09-10 22:23:12 -0400

    Add auth interceptor for http requests

--- frontend/Shelf/src/app/auth/auth.interceptor.ts
+++ frontend/Shelf/src/app/auth/auth.interceptor.ts
@@ -1,23 +1,22 @@
+import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpRequest, HttpHandler } from '@angular/common/http';
-import { Injectable, Inject } from '@angular/core';
 import { AuthService } from '../servies/auth.service';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-
-    constructor(private authService: AuthService ) {}
+    constructor(private authService: AuthService) {}
 
     intercept(req: HttpRequest<any>, next: HttpHandler) {
-        const token = this.authService.getAuthToken();
-        if (token) {
-            // logged in
-            return next.handle(
-                req.clone({
-                    headers: req.headers.set('token', token)
-                })
-            );
+        const authToken = this.authService.getAuthToken();
+        if (authToken) {
+            const authRequest = req.clone({
+                headers: req.headers.set('token', authToken)
+            });
+            return next.handle(authRequest);
+
         } else {
-            return next.handle(req.clone());
+            const authRequest = req.clone();
+            return next.handle(authRequest);
         }
     }
 }
