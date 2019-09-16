e404fe2bb6b8cf41455a16e1a1e7c9937a8fc228
Connor Todd <todd37@purdue.edu>
2019-09-16 17:10:57 -0400

    Add form validation form username and password for login

--- frontend/Shelf/src/app/login/login/login.component.ts
+++ frontend/Shelf/src/app/login/login/login.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { LoginService } from './login.service';
 import { Router } from '@angular/router';
 import { AuthData } from '../../models/auth.data.model';
-import {MessageService} from "primeng/api";
+import { MessageService } from 'primeng/api';
+import { NgForm, FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -18,59 +19,67 @@ export class LoginComponent implements OnInit {
   isRegistering: boolean;
   birthday: string;
   subtitle: string;
+  loginForm: FormGroup;
+  loginSubmit = false;
+  registerForm: FormGroup;
+  registerSubmit = false;
 
-  constructor(private router: Router, public loginService: LoginService, private messageService: MessageService) { }
+
+  // tslint:disable-next-line: max-line-length
+  constructor(private router: Router, public loginService: LoginService, private messageService: MessageService, private formBuilder: FormBuilder) {
+    if (this.loginService.getAuthToken()) {
+      this.router.navigate(['/home']);
+    }
+  }
 
   ngOnInit() {
     this.subtitle = 'Login!';
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
   }
 
   registerUser() {
 
     this.subtitle = 'Sign up!';
 
-    const authData: AuthData = {username: this.username, password: this.password, email: this.email, birthday: this.birthday};
+    const authData: AuthData = { username: this.username, password: this.password, email: this.email, birthday: this.birthday };
 
     if (this.isRegistering) {
       this.loginService.registerUser(authData).subscribe(
-        () => {this.showRegistrationSuccess()},
-          error => {this.showError(error.error.message)},
-        );
-
+        () => { this.showRegistrationSuccess(); },
+        error => { this.showError(error.error.message); },
+      );
     } else {
-        this.isRegistering = !this.isRegistering;
-      }
-
+      this.isRegistering = !this.isRegistering;
+    }
   }
 
 
-  loginUser() {
+  loginUser(form: NgForm) {
+    this.loginSubmit = true;
     this.subtitle = 'Login!';
-
+    if (this.loginForm.invalid) {
+      return;
+    }
     if (!this.isRegistering) {
-
-      this.loginService.loginUser(this.username, this.password).subscribe(
+      this.loginService.loginUser(form.value.username, form.value.password).subscribe(
         response => {
-          console.log(response)
         },
-        error => {
-          this.showError(error.error.message);
-        }
-
+        error => {this.showError(error.error.message); }
       );
-
     } else {
       this.isRegistering = !this.isRegistering;
     }
   }
 
   showError(errorMessage = 'All fields must be filled out') {
-    this.messageService.add({severity: 'error', summary: 'Error!', detail: errorMessage});
+    this.messageService.add({ severity: 'error', summary: 'Error!', detail: errorMessage });
   }
 
   showRegistrationSuccess(successMessage = 'Registration successful!') {
-    this.messageService.add({severity: 'success', summary: 'Success!', detail: successMessage});
+    this.messageService.add({ severity: 'success', summary: 'Success!', detail: successMessage });
   }
-
 
 }
