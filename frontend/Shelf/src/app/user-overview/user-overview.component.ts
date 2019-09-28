e2cdbf754cf1603f5bd3cd0076cbbcfb3b1e914e
benkahlert <bkahlert@purdue.edu>
2019-09-28 11:12:52 -0400

    User component actually goes to profile

--- frontend/Shelf/src/app/user-overview/user-overview.component.ts
+++ frontend/Shelf/src/app/user-overview/user-overview.component.ts
@@ -1,22 +1,24 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-overview',
   templateUrl: './user-overview.component.html',
   styleUrls: ['./user-overview.component.scss']
 })
+
 export class UserOverviewComponent implements OnInit {
 
   @Input() username: string;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
 
   }
 
   goToProfile = () => {
-    console.log('Going to the profile!');
+    this.router.navigate([`/profile/${this.username}`]);
   }
 
 }
