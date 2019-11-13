86aedafeb64e1cd25cca2e578ba97342447f271a
Connor Todd <todd37@purdue.edu>
2019-11-13 17:29:23 -0500

    add middleware to update other user's feeds upon certain events

---
+++ backend/middleware/updateFeed.js
@@ -0,0 +1,51 @@
+let mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_HOST, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('debug', false);
+
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+/* Objects */
+var User = require('../model/user');
+
+const USER_RATED_GAME_FEED = (user, game, rating) => {
+    return (user + ' has just rated the game \'' + game + ' ' + rating + '\' stars');
+};
+
+const USER_FOLLOWED_SOMEONE_ELSE_FEED = (follower, followee) => {
+    return (follower + ' has just begun following \'' + followee + '\'');
+};
+
+const WISH_LIST_FEED = (user, game) => {
+    return (user + 'has just added \'' + game + '\' to their wish list!');
+};
+
+const PLAYED_GAME_FEED = (user, game) => {
+    return (user + 'has just played \'' + game + '\'!');
+};
+
+const updateFeed = function(user, event) {
+
+    const followers = user.followers
+    console.log(followers)
+    followers.forEach(element => {
+        User.findOneAndUpdate({username: element}, {
+            $push: {
+                feed: {
+                    event: event,
+                    user: user.username
+                }
+            }
+        }).catch(err => {
+            console.log(err)
+            return;
+        })
+    })
+}
+
+module.exports = {updateFeed, USER_FOLLOWED_SOMEONE_ELSE_FEED, USER_RATED_GAME_FEED, WISH_LIST_FEED, PLAYED_GAME_FEED};
--- backend/routes/ratingInfo.js
+++ backend/routes/ratingInfo.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 let mongoose = require('mongoose');
 var authenticate = require('../middleware/authenticate');
+const updateFeed = require('../middleware/updateFeed')
 
 mongoose.connect(process.env.MONGODB_HOST, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.set('useNewUrlParser', true);
@@ -189,7 +190,7 @@ router.post("/:gameId", authenticate, async (req, res) => {
     //not in db
     RatingInfo.findOne({ game_id: req.params.gameId }).then(ratingInfo => {
 
-        console.log(ratingInfo)
+        // console.log(ratingInfo)
 
         if (!ratingInfo) {
             var newRatingInfo = new RatingInfo({
@@ -208,6 +209,7 @@ router.post("/:gameId", authenticate, async (req, res) => {
                             total_rating_value: req.body.newRating
                         }
                     }).exec().then(usr => {
+                        updateFeed.updateFeed(req.user, updateFeed.USER_RATED_GAME_FEED(req.user.username, req.body.gameName, req.body.newRating))
                         res.status(200).send({
                             message: req.body.gameId + " added to ratingInfo list for first" +
                                 " time!1"
@@ -230,6 +232,7 @@ router.post("/:gameId", authenticate, async (req, res) => {
                         res.status(200).send({ message: req.body.gameId + " removed rating info1" })
                         return
                     }).catch((err) => {
+                        console.log(err)
                         res.status(500).send(err);
                         return
                     })
@@ -242,9 +245,11 @@ router.post("/:gameId", authenticate, async (req, res) => {
                             total_rating_value: (req.body.newRating - req.body.oldRating)
                         }
                     }).then(usr => {
+                        updateFeed.updateFeed(req.user, updateFeed.USER_RATED_GAME_FEED(req.user.username, req.body.gameName, req.body.newRating))
                         res.status(200).send({ message: req.body.gameId + " updated to previous rating info1" })
                         return
                     }).catch((err) => {
+                        console.log(err)
                         res.status(500).send(err);
                         return
                     })
@@ -259,9 +264,11 @@ router.post("/:gameId", authenticate, async (req, res) => {
                         total_rating_value: req.body.newRating
                     }
                 }).exec().then(usr => {
+                    updateFeed.updateFeed(req.user, updateFeed.USER_RATED_GAME_FEED(req.user.username, req.body.gameName, req.body.newRating))
                     res.status(200).send({ message: req.body.gameId + " added to rating info for first time2!" })
                     return
                 }).catch((err) => {
+                    console.log(err)
                     res.status(500).send(err);
                     return
                 })
@@ -277,6 +284,7 @@ router.post("/:gameId", authenticate, async (req, res) => {
                     res.status(200).send({ message: req.body.gameId + " removed from rating info 2" })
                     return
                 }).catch((err) => {
+                    console.log(err)
                     res.status(500).send(err);
                     return
                 })
@@ -289,15 +297,18 @@ router.post("/:gameId", authenticate, async (req, res) => {
                         total_rating_value: (req.body.newRating - req.body.oldRating)
                     }
                 }).then(usr => {
+                    updateFeed.updateFeed(req.user, updateFeed.USER_RATED_GAME_FEED(req.user.username, req.body.gameName, req.body.newRating))
                     res.status(200).send({ message: req.body.gameId + " updated previous rating info 2" })
                     return
                 }).catch((err) => {
+                    console.log(err)
                     res.status(500).send(err);
                     return
                 })
             }
         }
     }).catch((err) => {
+        console.log(err)
         res.status(500).send(err);
         return;
     });
