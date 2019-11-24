0e8929d302972000480840b5150205466a9e4707
benkahlert <bkahlert@purdue.edu>
2019-11-24 12:19:39 -0500

    Add responsive add/remove buttons

--- frontend/Shelf/src/app/detailed-game/detailed-game.component.ts
+++ frontend/Shelf/src/app/detailed-game/detailed-game.component.ts
@@ -4,6 +4,7 @@ import { GamesService } from '../games/games.service';
 import { COVER_BIG, SCREENSHOT_BIG } from '../constants/constants.images';
 import { Location } from '@angular/common';
 import { typeWithParameters } from '@angular/compiler/src/render3/util';
+import { UserService } from '../user/user.service';
 
 @Component({
   selector: 'app-detailed-game',
@@ -21,8 +22,10 @@ export class DetailedGameComponent implements OnInit {
   userRating: number;
   comments;
   gameName: string;
+  isWishListed: boolean;
 
-  constructor(private route: ActivatedRoute, private gamesService: GamesService, private location: Location) {
+  constructor(private route: ActivatedRoute, private gamesService: GamesService,
+    private userService: UserService, private location: Location) {
     this.route.params.subscribe(params => this.id = params.id);
     this.comments = [];
   }
@@ -40,9 +43,21 @@ export class DetailedGameComponent implements OnInit {
       this.getDetailedGameData(true);
       this.getComments();
     }
+    this.getIsWishListed();
 
     this.coverPath = COVER_BIG;
     this.screenshotPath = SCREENSHOT_BIG;
+  }
+
+  getIsWishListed() {
+    const username = localStorage.getItem('user');
+    this.userService.fetchUser(username).subscribe(response => {
+      if (response.wish_list.includes(this.id)) {
+        this.isWishListed = true;
+      } else {
+        this.isWishListed = false;
+      }
+    })
   }
 
   getComments(){
@@ -167,11 +182,15 @@ export class DetailedGameComponent implements OnInit {
   }
 
   addToWishList() {
-    this.gamesService.addToWishList(this.id);
+    this.gamesService.addToWishList(this.id).then(x => {
+      this.isWishListed = true;
+    });
   }
 
   removeFromWishList() {
-    this.gamesService.removeFromWishList(this.id);
+    this.gamesService.removeFromWishList(this.id).then(x => {
+      this.isWishListed = false;
+    });
   }
 
 }
