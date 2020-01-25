e046ca73f656a88d9231163337be80a7683acdeb
Zach Johnson <john1861@purdue.edu>
2020-01-24 23:44:28 -0500

    Add gif to list

--- backend/middleware/photoUpload.js
+++ backend/middleware/photoUpload.js
@@ -11,7 +11,7 @@ cloudinary.config({
 const storage = cloudinaryStorage({
     cloudinary: cloudinary,
     folder: "GameImages",
-    allowedFormats: ["jpg", "png"],
+    allowedFormats: ["jpg", "png", "gif"],
     transformation: [{ width: 500, height: 500, crop: "limit" }]
 });
 
